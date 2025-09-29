4ac1ee170650b89ad11155d695d85a8fe7281511
Gilles M <10937624+gem85247@users.noreply.github.com>
2025-09-29 12:25:32 +0200

    Update intl_phone_number_input.dart

--- lib/intl_phone_number_input.dart
+++ lib/intl_phone_number_input.dart
@@ -3,4 +3,6 @@ library intl_phone_number_input;
 export 'src/utils/phone_number.dart';
 export 'src/widgets/input_widget.dart';
 export 'src/utils/selector_config.dart';
-export 'src/providers/country_provider.dart.dart';
+export 'src/providers/country_provider.dart';
+export 'src/models/country_model.dart';
+export 'src/models/country_list.dart';
